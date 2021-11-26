b231b9f31b13b731abd0ff3a385e19945559f974
Shirley Leung <sleung@usgs.gov>
2021-11-26 11:56:12 -0800

    changed = to <- where appropriate; changed absolute dir path to relative dir path

--- main.R
+++ main.R
@@ -5,12 +5,13 @@ library(stringr)
 library(sbtools)
 library(whisker)
 
-outer_dir <- 'C:/Users/SLEUNG/Github/ds-pipelines-targets-1'
+# REQUIREMENT: working dir must be same as dir where main.R is located
+outer_dir <- getwd()
 
 print('source the following files:')
 for (dir in list('1_fetch', '2_process', '3_visualize')) {
   setwd(file.path(outer_dir, dir, 'src'))
-  src_files = list.files(pattern='\\.R$')
+  src_files <- list.files(pattern='\\.R$')
   print(paste('in ', getwd(), ': ', sep=""))
   print(src_files)
   sapply(src_files, source)
@@ -18,7 +19,7 @@ for (dir in list('1_fetch', '2_process', '3_visualize')) {
 
 setwd(outer_dir)
 
-sbid = '5d925066e4b0c4f70d0d0599' 
+sbid <- '5d925066e4b0c4f70d0d0599' 
 
 data_filepath <- get_data_from_sb(sbid, outer_dir)
 data <- process_data_from_sb(data_filepath, outer_dir)
